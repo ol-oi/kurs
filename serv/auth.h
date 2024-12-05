2899a6274066dbcb4e24d8267929a6bd422a64b1
ol-oi <skyline839201@gmail.com>
2024-12-05 16:07:09 +0300

    Update auth.h

--- serv/auth.h
+++ serv/auth.h
@@ -4,8 +4,11 @@
 #include <unordered_map>
 #include <string>
 
-std::unordered_map<std::string, std::string> loadUsers(const std::string& filename);
-std::string generateSalt();
-std::string hashPassword(const std::string& salt, const std::string& password);
+class Auth {
+public:
+    static std::unordered_map<std::string, std::string> loadUsers(const std::string& filename);
+    static std::string generateSalt();
+    static std::string hashPassword(const std::string& salt, const std::string& password);
+};
 
 #endif // AUTH_H
