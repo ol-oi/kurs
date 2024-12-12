7b8af49b022b23379eb693a03544ddb363e36aad
ol-oi <skyline839201@gmail.com>
2024-12-12 17:49:33 +0300

    Add files via upload

---
+++ servak/auth.h
@@ -0,0 +1,20 @@
+#ifndef AUTH_H
+#define AUTH_H
+
+#include <string>
+
+class Auth {
+public:
+    // Конструктор
+    Auth();
+
+    // Функция для авторизации
+    bool login(const std::string& username, const std::string& password);
+
+private:
+    // Статические данные для проверки логина и пароля
+    static const std::string validUsername;
+    static const std::string validPassword;
+};
+
+#endif // AUTH_H
---
+++ servak/calc.cpp
@@ -0,0 +1,13 @@
+#include "calc.h"
+
+double Calculator::calculate_average(const std::vector<double>& vec) {
+    if (vec.empty()) {
+        return 0.0; // Возвращаем 0, если вектор пустой
+    }
+
+    double sum = 0.0;
+    for (double value : vec) {
+        sum += value;
+    }
+    return sum / vec.size();
+}
